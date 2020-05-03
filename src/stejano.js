ae5136ca747b5cbc76e464133267a6550a087599
Cameron Chitsaz <camcorder09@gmail.com>
2020-05-03 15:32:25 -0700

    got something working

--- src/stejano.js
+++ src/stejano.js
@@ -1,7 +1,20 @@
-function conceal(msg, img) {
-    console.log('concealing...');
+var Jimp = require('jimp');
 
-    return true;
+function conceal(msg, img, callback) {
+
+  console.log("msg is: " + msg);
+  console.log("img is: " + img);
+
+  Jimp.read(img)
+    .then(image => {
+      console.log(JSON.stringify(image));
+      console.log(image.bitmap.data.length);
+      callback(true);
+    })
+    .catch(err => {
+      console.log("there was an error", error);
+    });
+
 }
 
 module.exports = conceal;
--- test/stejano.test.js
+++ test/stejano.test.js
@@ -1,5 +1,17 @@
 const conceal = require('../src/stejano.js');
 
-test('basic test', () => {
-    expect(conceal('msg', 'image.jpeg')).toBe(true);
-})
+test('basic test', done => {
+  function callback(data) {
+    try {
+      expect(data).toBe(true);
+      done();
+    } catch (error) {
+      done(error);
+    }
+  }
+
+    //let result = await conceal('msg', './test-files/image.jpg');
+    //expect(result).toBe(true);
+    //expect(conceal('msg', './test-files/image.jpg')).toBe(true);
+    conceal('msg', './test-files/image.jpg', callback);
+});
