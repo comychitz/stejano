da3ac03ceb97bf6c911cb035565ab3579f436135
Cameron Chitsaz <camcorder09@gmail.com>
2020-05-09 12:32:18 -0700

    ascii -> binary && binary -> ascii util functions

--- src/util.js
+++ src/util.js
@@ -9,6 +9,19 @@ function asciiStrToBinary(str) {
       asciiCodeForChar = asciiCodeForChar >> 1;
     }
     binaryOfString = binaryOfString.concat(binaryOfChar);
+    binaryOfChar.length = 0;
   }
   return binaryOfString;
+}
+
+function binaryToAsciiStr(bin) {
+  let str = "";
+  for (i = 0; i < bin.length; i += 8) {
+    let charBinaryStr = "";
+    for (j = 0; j < 8; j++) {
+      charBinaryStr += bin[i + j];
+    }
+    str += String.fromCharCode(parseInt(charBinaryStr, 2));
+  }
+  return str;
 }
