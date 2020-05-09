464841bb4d68b9dd17fafd49e8d6e7517cb46958
Cameron Chitsaz <camcorder09@gmail.com>
2020-05-09 12:17:17 -0700

    add max msg size util functio

--- src/stejano.js
+++ src/stejano.js
@@ -7,14 +7,17 @@ function conceal(msg, img, callback) {
 
   Jimp.read(img)
     .then(image => {
-      console.log(JSON.stringify(image));
+      console.log("max msg length allowed: " + maxMsgSize(image.bitmap.width, image.bitmap.height));
       console.log(image.bitmap.data.length);
       callback(true);
     })
     .catch(err => {
       console.log("there was an error", error);
     });
+}
 
+function maxMsgSize(width, height) {
+  return (width * height * 3 / 8);
 }
 
 module.exports = conceal;
